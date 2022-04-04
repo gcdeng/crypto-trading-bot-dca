063098332c85de9ad9ab0e7a6da8107c3e726b55
gcdeng <ericdenggc@gmail.com>
2022-04-04 20:43:55 +0800

    add grantTotalAmount option: 'all'

--- parameters.js
+++ parameters.js
@@ -1,3 +1,8 @@
+/**
+ * This is a example order parameters represents to purchase cryptocurrencies by each percentage in portfolio with 400 USDT.
+ * In this example, you must have at least 400 USDT in your account to ensure the bot works as expected.
+ */
+
 // baseCurrency and percentage
 const portfolio = {
   BTC: 20,
@@ -13,6 +18,6 @@ const portfolio = {
 
 const quoteCurrency = "USDT";
 
-const grantTotalAmount = 400; // grant total available balance of quoteCurrency in your account if grantTotalAmount set to 0
+const grantTotalAmount = 400; // grant total available balance of quoteCurrency in your account if grantTotalAmount set to 'all'
 
 module.exports = { portfolio, quoteCurrency, grantTotalAmount };
