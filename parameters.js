a34634d4b82f1878370b221ddaecb5ba61061e9a
gcdeng <ericdenggc@gmail.com>
2022-04-22 22:00:50 +0800

    update parameters

--- parameters.js
+++ parameters.js
@@ -7,14 +7,15 @@
 // set crypto symbol and percentage you want to buy
 const portfolio = {
   BTC: 20,
-  ETH: 20,
-  BNB: 10,
-  FTT: 10,
+  ETH: 16,
+  BNB: 8,
+  FTT: 8,
   SOL: 8,
   LUNA: 8,
   AVAX: 8,
   DOT: 8,
   ATOM: 8,
+  NEAR: 8,
 };
 
 // your preferred stablecoin symbol for payment
