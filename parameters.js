6ccaf482ce669c02f4948ff046a6449769c90278
gcdeng <ericdenggc@gmail.com>
2022-05-27 16:54:52 +0800

    update parameters

--- parameters.js
+++ parameters.js
@@ -1,26 +1,19 @@
 /**
- * This is a example order parameters represents to purchase cryptocurrencies by each percentage in portfolio with 400 USDT.
+ * This is a example order parameters represents to purchase cryptocurrencies by each percentage in portfolio with {grantTotalAmount} USDT.
  *
- * if you don't have sufficient balance (less than 400 USDT in this example) in your account then the purchase will fail. The bot will try to make another purchase on the next scheduled time.
+ * if you don't have sufficient balance (less than {grantTotalAmount} USDT in this example) in your account then the purchase will fail. The bot will try to make another purchase on the next scheduled time.
  */
 
 // set crypto symbol and percentage you want to buy
 const portfolio = {
-  BTC: 30,
-  ETH: 20,
-  BNB: 10,
-  FTT: 10,
-  SOL: 6,
-  AVAX: 6,
-  DOT: 6,
-  ATOM: 6,
-  NEAR: 6,
+  BTC: 50,
+  ETH: 50,
 };
 
 // your preferred stablecoin symbol for payment
 const quoteCurrency = "USDT";
 
 // how much you want to buy
-const grantTotalAmount = 300; // grant total available balance of quoteCurrency in your account if grantTotalAmount set to 'all'
+const grantTotalAmount = 100; // grant total available balance of quoteCurrency in your account if grantTotalAmount set to 'all'
 
 module.exports = { portfolio, quoteCurrency, grantTotalAmount };
